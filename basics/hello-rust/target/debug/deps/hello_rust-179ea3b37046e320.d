c65a0cd85e91d6ccf37272b974d60da76151dce7
matiscodjia <codjiamatis01@gmail.com>
2025-03-17 22:45:14 +0100

    feat: my first application in RUST using default cargo app generator

---
+++ basics/hello-rust/target/debug/deps/hello_rust-179ea3b37046e320.d
@@ -0,0 +1,5 @@
+/Users/mtis/Local/Code/GitRepos/RustAI/basics/hello-rust/target/debug/deps/hello_rust-179ea3b37046e320: src/main.rs
+
+/Users/mtis/Local/Code/GitRepos/RustAI/basics/hello-rust/target/debug/deps/hello_rust-179ea3b37046e320.d: src/main.rs
+
+src/main.rs:
---
+++ basics/hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/Users/mtis/Local/Code/GitRepos/RustAI/basics/hello-rust/target/debug/hello-rust: /Users/mtis/Local/Code/GitRepos/RustAI/basics/hello-rust/src/main.rs
